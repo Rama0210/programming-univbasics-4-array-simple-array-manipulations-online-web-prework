71a794b247927d1b954f9d324000f4abf509d04c
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-09 02:15:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,8 +26,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array1, array2)
-  array1.insert(array2)
+def using_insert(array, array1)
+  array.insert(array1)
 end
 
 def using_uniq(array)
